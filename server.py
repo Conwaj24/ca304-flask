a189906ab68b225d1e423333edb5350cfa56bc78
JCoMcL <rapcyss@gmail.com>
2020-12-17 11:24:01 +0000

    complete bare minimum form and submit

--- server.py
+++ server.py
@@ -1,14 +1,30 @@
 #!/usr/bin/env pipenv run python
 from flask import Flask
-import os
+from flask import render_template
+from flask import request
 
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    with open('./index.html', 'r') as f:
-        return f.read()
+    return render_template('index.html')
+
+@app.route('/formtest')
+def form():
+    return render_template('form.html')
+
+@app.route('/formsubmit', methods=['POST'])
+def result():
+    return request.form['name']
+
+@app.route('/allegiances')
+def json():
+    return "allegiances"
+
+@app.route('/allegiancedashboard')
+def dash():
+    return "allegiancedashboard"
 
 if __name__ == "__main__":
-    app.run()
+    app.run(port=8000)
 
