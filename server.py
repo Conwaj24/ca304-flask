4c2704dfa75bf296c4aa7f044a17a222862b0863
JCoMcL <rapcyss@gmail.com>
2020-12-18 13:25:36 +0000

    add json encoding of allegiances

--- server.py
+++ server.py
@@ -2,6 +2,25 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+from flask import Response
+import os
+import json
+import csv
+
+def csv_to_dicts(csv):
+    fields = csv.readline().strip().split(',')
+    out = []
+    for line in csv:
+        d = {}
+        vals = line.strip().split(',')
+        for i in range(min(len(fields), len(vals))):
+            d[fields[i]] = vals[i]
+        out.append(d)
+    return out
+
+def csv_to_json(csv_filename):
+    with open("allegiance.csv", "r") as f:
+        return( json.dumps( csv_to_dicts(f)))
 
 app = Flask(__name__)
 
@@ -18,8 +37,8 @@ def result():
     return request.form['name']
 
 @app.route('/allegiances')
-def json():
-    return "allegiances"
+def rest():
+    return Response("allegiances", mimetype="application/json")
 
 @app.route('/allegiancedashboard')
 def dash():
